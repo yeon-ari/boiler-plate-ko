a030188fe7c51b19acef3d91788ebc70cffde8d6
김나연 <oyeah012@gmail.com>
2023-05-25 14:46:23 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key')
+
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://nayeon0729:1212@boilerplate.yyinpuz.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=>console.log('MongoDB Connected...'))
-
+.catch(err=>console.log(err))
 
 app.get('/',(req,res)=>res.send('Hello World!'))
+
+
+//회원가입을 위한 route
+app.post('/register', (req, res)=>{
+    //회원가입할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    try{
+        user.save()
+        .then(()=>{
+            return res.status(200).json({success:true});
+        })
+        .catch((err)=>{
+            return res.json({success:false,err});
+        })
+    } catch (err){
+        console.log(err);
+    }
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -8,25 +10,40 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         trim: true,
-        unique:1
+        unique:1    //똑같은 이메일을 쓰지 못하도록
     },
     password: {
         type: String,
         maxlength: 50
     },
-    role: {
+    role: { //유저는 관리자가 될 수도, 일반인이 될 수도
         type: Number,
-        default: 0
+        default: 0  //0: 일반 유저, 1: 관리자
     },
     image: String,
-    token: {
+    token: {    //유효성 관리
         type: String
     },
-    tokenExp: {
+    tokenExp: { //유효 기간
         type: Number
     }
 })
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', function(next){
+    var user = this;
+    if (user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports={User};
